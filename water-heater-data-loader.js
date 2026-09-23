f496c089acafd9983db3110bf153f8cc599ddca1
Uranian-Drift <a911714082@gmail.com>
2026-09-23 16:52:09 +0800

    更新5.1看板销售至2026-09-22

--- water-heater-data-loader.js
+++ water-heater-data-loader.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const version = "20260922-142856";
+  const version = "20260923-162130";
   const parts = [
     "water-heater-data-00.txt",
     "water-heater-data-01.txt",
